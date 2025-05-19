4c6090f188972c6815a45a2108d20cb539c88e8b
Anupam <48309157+usrspartan@users.noreply.github.com>
2025-05-20 00:00:20 +0530

    Time: 53 ms (6.96%) | Memory: 55 MB (5.02%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+        return undefined;
+    };
+};
